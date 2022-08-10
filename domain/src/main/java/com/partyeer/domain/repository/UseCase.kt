ca25403ea719e2f4b5f29c58cc74023b05b04917
Adnan BAL <adnbal89@hotmail.com>
2022-08-10 23:47:06 +0300

    Either.kt, Failure.kt, UseCase.kt implemented.

---
+++ domain/src/main/java/com/partyeer/domain/repository/UseCase.kt
@@ -0,0 +1,34 @@
+package com.partyeer.domain.repository
+
+import com.partyeer.util.exception.Failure
+import com.partyeer.util.functional.Either
+import kotlinx.coroutines.*
+
+/**
+ * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
+ * This abstraction represents an execution unit for different use cases (this means that any use
+ * case in the application should implement this contract).
+ *
+ * By convention each [UseCase] implementation will execute its job in a background thread
+ * (kotlin coroutine) and will post the result in the UI thread.
+ */
+abstract class UseCase<out Type, in Params> where Type : Any {
+
+    abstract suspend fun run(params: Params): Either<Failure, Type>
+
+    //Defaultscope : GlobalScope if not defined.
+    operator fun invoke(
+        params: Params,
+        scope: CoroutineScope = CoroutineScope(Dispatchers.Main),
+        onResult: (Either<Failure, Type>) -> Unit = {}
+    ) {
+        scope.launch(Dispatchers.Main) {
+            val deferred = async(Dispatchers.IO) {
+                run(params)
+            }
+            onResult(deferred.await())
+        }
+    }
+
+    class None
+}
