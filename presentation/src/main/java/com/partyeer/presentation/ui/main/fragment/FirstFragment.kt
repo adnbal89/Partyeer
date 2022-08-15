daf80f46ba9ef44fa4be80110c41207b81309aaf
Adnan BAL <adnbal89@hotmail.com>
2022-08-15 22:20:35 +0300

    BaseRecyclerViewHolder.kt implemented, inflaterextension implemented.

--- presentation/src/main/java/com/partyeer/presentation/ui/main/fragment/FirstFragment.kt
+++ presentation/src/main/java/com/partyeer/presentation/ui/main/fragment/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.partyeer.presentation.R
 import com.partyeer.presentation.databinding.FragmentFirstBinding
 import com.partyeer.presentation.ui.main.base.BaseFragment
---
+++ presentation/src/main/java/com/partyeer/presentation/ui/main/view/recycler/BaseRecyclerViewHolder.kt
@@ -0,0 +1,71 @@
+package com.partyeer.presentation.ui.main.view.recycler
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.partyeer.presentation.ui.main.extension.inflate
+
+
+/**
+ * A base ViewHolder that handles click and long click events.
+ */
+abstract class BaseRecyclerViewHolder<T> : RecyclerView.ViewHolder,
+    View.OnClickListener, View.OnLongClickListener {
+
+    val context: Context
+        get() = itemView.context
+
+    private var onItemClick: ((Int) -> Unit)? = null
+    private var onItemLongClick: ((Int) -> Unit)? = null
+
+    /**
+     * Constructor.
+     *
+     * @param parentView parent
+     * @param layoutId   layout resource to inflate
+     */
+    constructor(parentView: ViewGroup, @LayoutRes layoutId: Int)
+            : this(parentView.inflate(layoutId))
+
+    /**
+     * Constructor.
+     *
+     * @param itemView item view
+     */
+    constructor(itemView: View) : super(itemView)
+
+    /**
+     * Register a callback to be invoked when this item is clicked.
+     *
+     * @param onItemClick callback
+     */
+    fun setItemClickListener(onItemClick: (Int) -> Unit) {
+        this.onItemClick = onItemClick
+        this.itemView.setOnClickListener(this)
+    }
+
+    /**
+     * Register a callback to be invoked when this item is clicked and held.
+     *
+     * @param onItemLongClick callback
+     */
+    fun setItemLongClickListener(onItemLongClick: (Int) -> Unit) {
+        this.onItemLongClick = onItemLongClick
+        this.itemView.setOnLongClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        onItemClick?.invoke(bindingAdapterPosition)
+    }
+
+    override fun onLongClick(p0: View?): Boolean {
+        onItemLongClick?.invoke(bindingAdapterPosition)
+        return true
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+fun BaseRecyclerViewHolder<*>.any(): BaseRecyclerViewHolder<Any> =
+    this as BaseRecyclerViewHolder<Any>
