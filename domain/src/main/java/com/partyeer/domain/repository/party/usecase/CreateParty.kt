74526529f3f4148735179f261fd205f1f6b32b5a
Adnan BAL <adnbal89@hotmail.com>
2022-09-08 21:23:34 +0300

    İnsert Party use case implemented.

---
+++ domain/src/main/java/com/partyeer/domain/repository/party/usecase/CreateParty.kt
@@ -0,0 +1,20 @@
+package com.partyeer.domain.repository.party.usecase
+
+import com.partyeer.domain.repository.base.usecase.CoroutineUseCase
+import com.partyeer.domain.repository.party.PartyRepository
+import com.partyeer.domain.repository.party.model.Party
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CreateParty @Inject constructor(
+    private val repository: PartyRepository
+) : CoroutineUseCase<Party, Unit>() {
+
+
+    override suspend fun buildUseCase(params: Party?) {
+        return repository.insertParty(params)
+    }
+
+    class None
+}
