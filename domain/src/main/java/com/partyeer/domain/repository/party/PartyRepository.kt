2d5b6b128f251368ae7e67abdb1cc3a8e66c453f
Adnan BAL <adnbal89@hotmail.com>
2022-09-08 21:23:34 +0300

    İnsert Party use case implemented.

--- domain/src/main/java/com/partyeer/domain/repository/party/PartyRepository.kt
+++ domain/src/main/java/com/partyeer/domain/repository/party/PartyRepository.kt
@@ -2,8 +2,11 @@ package com.partyeer.domain.repository.party
 
 import com.partyeer.domain.repository.base.BaseRepository
 import com.partyeer.domain.repository.party.model.Party
+import javax.inject.Singleton
 
 interface PartyRepository : BaseRepository {
 
     suspend fun getParty(id: String): Party
+
+    suspend fun insertParty(party: Party)
 }
