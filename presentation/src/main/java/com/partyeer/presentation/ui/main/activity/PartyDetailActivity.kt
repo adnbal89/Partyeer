680474eed1b2f7537268e32adf4ef42a16f475f1
adnbal89 <adnbal89@gmail.com>
2022-10-26 19:57:45 +0300

    PartyDetailActivity.kt party pictures implemented.

--- presentation/src/main/java/com/partyeer/presentation/ui/main/activity/PartyDetailActivity.kt
+++ presentation/src/main/java/com/partyeer/presentation/ui/main/activity/PartyDetailActivity.kt
@@ -6,26 +6,35 @@ import com.partyeer.domain.repository.party.model.Party
 import com.partyeer.presentation.databinding.ActivityPartyDetailBinding
 import com.partyeer.presentation.ui.main.activity.viewmodel.PartyDetailViewModel
 import com.partyeer.presentation.ui.main.base.BaseActivity
+import com.partyeer.presentation.ui.main.features.party.createparty.PictureRecyclerViewAdapter
 
 class PartyDetailActivity : BaseActivity() {
     private val viewModel: PartyDetailViewModel by viewModels()
 
     private lateinit var binding: ActivityPartyDetailBinding
     private lateinit var party: Party
+    private val pictureRecyclerViewAdapter = PictureRecyclerViewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPartyDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.toolbar)
 
         party = intent.getParcelableExtra<Party>("party").let { it!! }
+
+        pictureRecyclerViewAdapter.setItems(party.pictures)
 
         binding.textViewStartTimeValue.text = party.timeStart.toString()
         binding.textViewLocationValue.text = party.address
         binding.textViewEndTimeValue.text = party.timeEnd.toString()
         binding.textViewDescriptionValue.text = party.description
-    }
 
+        with(binding) {
+            with(viewPagerPartyDetail) {
+                adapter = pictureRecyclerViewAdapter
+            }
+        }
+    }
 
 }
